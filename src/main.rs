605560e1fdd1c75e1bfc67eca3e9cf4bc13b98a3
ddki <ddki2068@gmail.com>
2024-01-06 03:02:47 +0800

    ✨feat: v0.0.1 done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+mod zip_utils;
+
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    path,
+};
+
 use clap::{arg, command, Parser};
+use git2::{DiffFormat, Oid};
+
+fn match_str(s1: &str, s2: &str) -> bool {
+    s1.eq(s2) || s1.contains(s2) || s1.to_lowercase().eq(&s2.to_lowercase())
+}
 
 #[derive(Parser, Debug)]
 #[command(name = "git-export")]
@@ -7,7 +21,7 @@ struct Args {
     #[arg(
         short = 'f',
         long = "filter",
-        help = "必填项，过滤，支持正则语法，支持tag,username,email,commit message..."
+        help = "必填项，过滤，支持username,email,commit message..."
     )]
     filter: String,
 
@@ -15,16 +29,42 @@ struct Args {
         short = 'o',
         long = "outdir",
         default_value = "git-export",
-        help = "导出目录，默认当前文件夹/git-export"
+        help = "导出目录"
     )]
     out_dir: Option<String>,
 
-    #[arg(long = "commit", help = "commit哈希，filter限定在这些commit中")]
-    commits: Vec<String>,
+    #[arg(
+        long = "in-commit",
+        help = "commit哈希，filter限定在这些commit中，多个可以使用逗号分隔"
+    )]
+    in_commits: Vec<String>,
+
+    #[arg(long = "zip", default_value = "source.zip", help = "是否打包成zip文件")]
+    zip: String,
 }
 
 fn main() {
     let args = Args::parse();
+
+    let filter = args.filter;
+    let out_dir = match args.out_dir {
+        Some(s) => s,
+        None => return,
+    };
+    let in_commits = args.in_commits;
+    let zip = args.zip;
+
+    if !zip.ends_with(".zip") {
+        panic!("zip 文件名称有误，请使用.zip结尾");
+    }
+
+    let current_dir = env::current_dir().unwrap();
+    let out_path = current_dir.join(&out_dir);
+
+    println!(
+        "filter = {}, out_dir = {:?}, in_commits = {:?}",
+        filter, out_dir, in_commits
+    );
 
     let repo = match git2::Repository::open("./") {
         Ok(repo) => repo,
@@ -33,5 +73,90 @@ fn main() {
 
     println!("repo path = {:?}", repo.path());
 
-    println!("{:?}", args);
+    let mut revwalk = repo.revwalk().unwrap();
+
+    // 设置遍历开始的提交
+    revwalk.push_head().unwrap();
+
+    // 使用 `filter_map` 方法来筛选并映射提交
+    let result: Vec<Oid> = revwalk
+        .filter_map(|oid| {
+            let commit = repo.find_commit(oid.unwrap()).unwrap();
+            let message = commit.message().unwrap();
+            let author_binding = commit.author();
+            let author_name = author_binding.name().unwrap();
+            let author_email = author_binding.email().unwrap();
+
+            let match_filter = match_str(message, &filter)
+                || match_str(author_name, &filter)
+                || match_str(author_email, &filter);
+
+            if in_commits.is_empty() {
+                if match_filter {
+                    Some(commit.id())
+                } else {
+                    None
+                }
+            } else {
+                if in_commits.contains(&commit.id().to_string()) {
+                    Some(commit.id())
+                } else {
+                    None
+                }
+            }
+        })
+        .collect();
+
+    // 打印符合条件的提交
+    for oid in result {
+        let commit = repo.find_commit(oid).unwrap();
+        let commit_tree = commit.tree().unwrap();
+
+        println!("author: {}", commit.author().name().unwrap());
+        println!("email: {}", commit.author().email().unwrap());
+        println!("message: {}", commit.message().unwrap().trim());
+        println!("tree: {:?}", commit_tree);
+        println!("id: {}", commit.id());
+
+        for parent in commit.parents() {
+            let parent_tree = parent.tree().unwrap();
+
+            let diff = repo
+                .diff_tree_to_tree(Some(&parent_tree), Some(&commit_tree), None)
+                .unwrap();
+
+            diff.print(DiffFormat::Patch, |_delta, _hunk, _line| {
+                let new_file = _delta.new_file();
+                let new_file_path = match new_file.path() {
+                    Some(path) => path
+                        .display()
+                        .to_string()
+                        .replace("/", path::MAIN_SEPARATOR_STR)
+                        .replace("\\", path::MAIN_SEPARATOR_STR),
+                    None => todo!(),
+                };
+                let new_file_blob = repo.find_blob(new_file.id()).unwrap();
+                let new_file_content = std::str::from_utf8(new_file_blob.content()).unwrap();
+
+                let binding = out_path.join(new_file_path);
+                let file_path = binding.as_path();
+
+                let file_dir = file_path.parent().ok_or("No Parent Directory").unwrap();
+                if !file_dir.exists() {
+                    fs::create_dir_all(file_dir).expect("create dir failed");
+                }
+
+                let mut file = File::create(file_path).expect("create file failed");
+                file.write_all(new_file_content.as_bytes())
+                    .expect("diff file write failed");
+
+                true
+            })
+            .unwrap();
+        }
+    }
+
+    if !zip.is_empty() {
+        zip_utils::ZipUtils::zip_dir_to_file(out_path.to_str().unwrap(), &zip);
+    }
 }
---
+++ src/zip_utils.rs
@@ -0,0 +1,75 @@
+use std::{
+    fs::File,
+    io::{Read, Seek, Write},
+    path::Path,
+};
+use walkdir::{DirEntry, WalkDir};
+
+use zip::{result::ZipError, write::FileOptions};
+
+pub struct ZipUtils {}
+
+impl ZipUtils {
+    pub fn zip_dir_to_file(dir: &str, filename: &str) {
+        match Self::doit(dir, filename, zip::CompressionMethod::Bzip2) {
+            Ok(_) => println!("done: {dir} written to {filename}"),
+            Err(e) => println!("Error: {e:?}"),
+        }
+    }
+
+    fn zip_dir<T>(
+        it: &mut dyn Iterator<Item = DirEntry>,
+        prefix: &str,
+        writer: T,
+        method: zip::CompressionMethod,
+    ) -> zip::result::ZipResult<()>
+    where
+        T: Write + Seek,
+    {
+        let mut zip = zip::ZipWriter::new(writer);
+        let options = FileOptions::default()
+            .compression_method(method)
+            .unix_permissions(0o755);
+
+        let mut buffer = Vec::new();
+        for entry in it {
+            let path = entry.path();
+            let name = path.strip_prefix(Path::new(prefix)).unwrap();
+
+            if path.is_file() {
+                #[allow(deprecated)]
+                zip.start_file_from_path(name, options)?;
+                let mut f = File::open(path)?;
+
+                f.read_to_end(&mut buffer)?;
+                zip.write_all(&buffer)?;
+                buffer.clear();
+            } else if !name.as_os_str().is_empty() {
+                #[allow(deprecated)]
+                zip.add_directory_from_path(name, options)?;
+            }
+        }
+        zip.finish()?;
+        Result::Ok(())
+    }
+
+    fn doit(
+        src_dir: &str,
+        dst_file: &str,
+        method: zip::CompressionMethod,
+    ) -> zip::result::ZipResult<()> {
+        if !Path::new(src_dir).is_dir() {
+            return Err(ZipError::FileNotFound);
+        }
+
+        let path = Path::new(dst_file);
+        let file = File::create(path).unwrap();
+
+        let walkdir = WalkDir::new(src_dir);
+        let it = walkdir.into_iter();
+
+        Self::zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
+
+        Ok(())
+    }
+}
